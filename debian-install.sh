086cf46c38ecae849276136a135878b8f6d6e1dd
ajedptr <aejdptriii@gmail.com>
2024-11-16 13:00:57 +0000

    Add debian-install.sh for Debian installation

---
+++ debian-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker --version
+echo "Docker installation completed on Debian!"
